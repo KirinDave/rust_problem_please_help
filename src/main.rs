a6e2c81cfb6d666d7b12a6a67b36064e3f0a2548
kirindave <dfayram@gmail.com>
2022-12-28 20:42:43 -0800

    Thanks to @thombles@aus.social for this suggestion and explaining why it works this way.\nSee: https://mastodon.social/@thombles@aus.social/109595077496056201

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use google_cloud_gax::cancel::CancellationToken;
 use google_cloud_gax::grpc::Status;
-use google_cloud_googleapis::pubsub::v1::PubsubMessage;
 use google_cloud_pubsub::client::Client;
 use google_cloud_pubsub::subscription::SubscriptionConfig;
 use std::time::Duration;
@@ -12,7 +11,7 @@ async fn main() -> Result<(), Status> {
     // - If the GOOGLE_APPLICATION_CREDENTIALS is specified the project_id is from credentials.
     // - If the server is running on GCP the project_id is from metadata server
     // - If the PUBSUB_EMULATOR_HOST is specified the project_id is 'local-project'
-    let mut client = Client::default().await.unwrap();
+    let client = Client::default().await.unwrap();
 
     // Get the topic to subscribe to.
     let topic = client.topic("test-topic");
@@ -40,29 +39,33 @@ async fn main() -> Result<(), Status> {
         cancel2.cancel();
     });
 
-    let (sender, receiver) = tokio::sync::mpsc::channel::<String>(64);
+    let (sender, _receiver) = tokio::sync::mpsc::channel::<String>(64);
     // Receive blocks until the ctx is cancelled or an error occurs.
     // Or simply use the `subscription.subscribe` method.
 
     subscription
         .receive(
-            move |mut message, cancel| async move {
-                // Handle data.
-                let data = message.message.data.as_slice();
-                let data_string = String::from_utf8(message.message.data.clone()).unwrap();
-                sender.clone().send(data_string).await.unwrap();
-                println!("{:?}", data);
+            move |message, _cancel| {
+                let sender = sender.clone();
+                async move {
+                    // Handle data.
+                    let data = message.message.data.as_slice();
+                    let data_string = String::from_utf8(message.message.data.clone()).unwrap();
+                    sender.send(data_string).await.unwrap();
+                    println!("{:?}", data);
 
-                // Ack or Nack message.
-                message.ack().await;
+                    // Ack or Nack message.
+                    message.ack().await.unwrap();
+                }
             },
             cancel.clone(),
             None,
         )
-        .await;
+        .await
+        .unwrap();
 
     // Delete subscription if needed.
-    subscription.delete(None, None).await;
+    subscription.delete(None, None).await.unwrap();
 
     Ok(())
 }
